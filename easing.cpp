41fe6dc1b8214792911b75cf71684b17fe5948a3
Jelegk <legault.jeremy18@gmail.com>
2025-07-27 01:39:44 -0400

    operation on ‘x’ may be undefined [-Wsequence-point]

--- easing.cpp
+++ easing.cpp
@@ -149,11 +149,16 @@ static double easeInOutElastic(const double x) {
 static double easeOutBounce(double x) {
     if (x < 1 / d1)
         return n1 * x * x;
-    if (x < 2 / d1)
-        return n1 * (x -= 1.5 / d1) * x + 0.75;
-    if (x < 2.5 / d1)
-        return n1 * (x -= 2.25 / d1) * x + 0.9375;
-    return n1 * (x -= 2.625 / d1) * x + 0.984375;
+    if (x < 2 / d1) {
+        x -= 1.5;
+        return n1 * (x / d1) * x + 0.75;
+    }
+    if (x < 2.5 / d1) {
+        x -= 2.25;
+        return n1 * (x / d1) * x + 0.9375;
+    }
+    x -= 2.625;
+    return n1 * (x / d1) * x + 0.984375;
 }
 
 static double easeInBounce(const double x) {
